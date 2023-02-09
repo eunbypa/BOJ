1c99c7d9293ad438c9f498e71c59f1441e4ff5b5
CHOEUNBY <72124246+eunbypa@users.noreply.github.com>
2023-02-09 17:50:49 +0900

    [Silver II] Title: DNA 비밀번호, Time: 224 ms, Memory: 41516 KB -BaekjoonHub

---
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.io.*;
+// 백준 신기한 소수
+public class Main {
+	static int S,P;
+	static char[] dna;
+	static int[][] dp;
+	static int[] req;
+	static int ans;
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		S = Integer.parseInt(st.nextToken());
+		P = Integer.parseInt(st.nextToken());
+		dp = new int[S-P+1][4];
+		req = new int[4];
+		dna = br.readLine().toCharArray();
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < 4; i++) {
+			req[i] = Integer.parseInt(st.nextToken());
+		}
+		findPassword();
+		System.out.println(ans);
+	}
+
+	public static void findPassword() {
+		int s = 0;
+		int e = P-1;
+		//초기 상태 설정
+		for (int i = 0; i < P; i++) {
+			dp[s][getIdx(dna[i])]++;
+		}
+		if(isAns(s)) ans++;
+		while(e < S) {
+			s++;
+			e++;
+			if(e == S) break;
+			for (int i = 0; i < 4; i++) {
+				dp[s][i] = dp[s-1][i];
+			}
+			dp[s][getIdx(dna[s-1])]--;
+			dp[s][getIdx(dna[e])]++;
+			if(isAns(s)) ans++;
+		}
+	}
+	public static boolean isAns(int idx) {
+		for (int i = 0; i < 4; i++) {
+			if(req[i] > dp[idx][i]) return false;
+		}
+		return true;
+	}
+	public static int getIdx(char c) {
+		if(c == 'A') {
+			return 0;
+		}else if(c == 'C') {
+			return 1;
+		}else if(c == 'G') {
+			return 2;
+		}return 3;
+	}
+}
