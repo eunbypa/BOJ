fabd9d3a958f6d7700c2307448a4a0479e299541
CHOEUNBY <72124246+eunbypa@users.noreply.github.com>
2023-02-15 15:40:01 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 432 ms, Memory: 49924 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -1,9 +1,18 @@
 import java.util.*;
 import java.io.*;
+class Point{
+	int r;
+	int c;
+	public Point(int r, int c) {
+		this.r = r;
+		this.c = c;
+	}
+}
 public class Main {
-	static int N,M,R;
+	static int N,M,R,turns;
 	static int[][] nums;
 	static int[][] tmp;
+	static Point[][] list;
 	static int[] dr = {1,0,-1,0};
 	static int[] dc = {0,1,0,-1};
 	public static void main(String[] args) throws IOException {
@@ -14,46 +23,72 @@ public class Main {
 		N=Integer.parseInt(st.nextToken());
 		M=Integer.parseInt(st.nextToken());
 		R=Integer.parseInt(st.nextToken());
+		turns = (N > M) ? M/2 : N/2; 
 		nums = new int[N][M];
 		tmp = new int[N][M];
+		list = new Point[N/2][];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
 				nums[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		for (int k = 0; k < R; k++) {
-			rotate(0,0,0);
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < M; j++) {
-					nums[i][j] = tmp[i][j];
-				}
-			}
-		}
+		setList(0,0,0);
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				sb.append(nums[i][j]+" ");
+				sb.append(tmp[i][j]+" ");
 			}
 			sb.append("\n");
 		}
 		System.out.println(sb.toString());
 	}
-	public static void rotate(int r, int c, int turn) {
-		if(r == N/2) return;
+	public static void setList(int r, int c, int turn) {
+		if(r == turns) return;
+		int x = r, y = c;
 		int nr=r, nc=c;
+		int tot = 2*((N-2*turn)+(M-2*turn))-4;
+		list[turn] = new Point[tot];
+		int idx = 0;
+		list[turn][idx++] = new Point(nr,nc);
 		for (int i = 0; i < 4; i++) {
 			nr=r;
 			nc=c;
-			while(check(nr, nc, turn)) {
+			while(idx != tot && check(nr, nc, turn)) {
+				//System.out.println("1무한루프");
 				nr = nr+dr[i];
 				nc = nc+dc[i];
 				if(!check(nr, nc,turn)) break;
-				tmp[nr][nc] = nums[r][c];
+				list[turn][idx++] = new Point(nr,nc);
+				//System.out.println(idx);
 				r = nr;
 				c = nc;
 			}
 		}
-		rotate(r+1,c+1,turn+1);
+		int rotateCnt = R % tot; // 주기
+		int d = 0;
+		nr = x;
+		nc = y;
+		//System.out.println("현재 턴 : "+turn);
+		for (int i = rotateCnt; i < list[turn].length; i++) {
+			tmp[list[turn][i].r][list[turn][i].c] = nums[nr][nc];
+			//System.out.println(nr + " " + nc +"->"+list[turn][i].r + " " + list[turn][i].c);
+			if(!check(nr+dr[d],nc+dc[d],turn)) {
+				d++;
+				d %= 4;
+			}
+			nr = nr+dr[d];
+			nc = nc+dc[d];
+		}
+		for (int i = 0; i < rotateCnt; i++) {
+			tmp[list[turn][i].r][list[turn][i].c] = nums[nr][nc];
+			if(!check(nr+dr[d],nc+dc[d],turn)) {
+				d++;
+				d %= 4;
+			}
+			nr = nr+dr[d];
+			nc = nc+dc[d];
+		}
+		setList(x+1,y+1,turn+1);
 	}
 	public static boolean check(int r, int c, int turn) {
 		return r >=turn&& c >= turn && r < N-turn && c < M-turn;
