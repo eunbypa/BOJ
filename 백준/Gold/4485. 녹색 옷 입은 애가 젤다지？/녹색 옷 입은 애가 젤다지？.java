a501bed8843914d59e3da49e8dcf3232f66a54b1
CHOEUNBY <72124246+eunbypa@users.noreply.github.com>
2023-03-02 17:16:11 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 148 ms, Memory: 16160 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -4,60 +4,57 @@ import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
+ 
 public class Main {
-	static int N, min = 400000;
-	static int[][] map;
-	static int[][] dist;
-	static int[] dr = {-1,0,1,0};
-	static int[] dc = {0,-1,0,1};
+
+	public static int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		N = Integer.parseInt(br.readLine());
-		int t = 1;
-		do {
-			map = new int[N][N];
-			dist = new int[N][N];
-			min = 400000;
+		int tc = 0;
+
+		// N이 0이라면 종료
+		while(true) {
+			tc++;	//문제번호
+			int N = Integer.parseInt(br.readLine());
+			if(N==0) break;
+			//입력
+			int[][] map = new int[N][N];
+			int[][] minmap = new int[N][N];
 			for (int i = 0; i <N; i++) {
 				String str = br.readLine();
 				for (int j = 0, index = 0; j < N; j++, index+=2) {
 					map[i][j] = str.charAt(index)-'0';
-					dist[i][j] = Integer.MAX_VALUE;
+					minmap[i][j] = Integer.MAX_VALUE;
 				}
 			}
-			dijkstra();
-			sb.append("Problem " + t+": "+dist[N-1][N-1]+"\n");
-			t++;
-			N = Integer.parseInt(br.readLine());
-		}while(N != 0);
-		System.out.println(sb.toString());
-	}
-	private static void dijkstra() {
-		PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2)->(o1[2]-o2[2]));
-		pq.offer(new int[] {0,0, map[0][0]});
-		dist[0][0] = map[0][0];
-		while(!pq.isEmpty()) {
-			int[] cur = pq.poll();
-			int r = cur[0];
-			int c = cur[1];
-			int cnt = cur[2];
-			if(dist[r][c] < cnt) continue;
-			for (int i = 0; i < 4; i++) {
-				int nr = r+dr[i];
-				int nc = c+dc[i];
-				if(!check(nr,nc)) continue;
-				if(dist[nr][nc] > cnt + map[nr][nc]) {
-					dist[nr][nc] =  cnt + map[nr][nc];
-					pq.offer(new int[] {nr,nc,dist[nr][nc]});
+			minmap[0][0] = map[0][0];
+			PriorityQueue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>() {
+
+				@Override
+				public int compare(int[] o1, int[] o2) {
+					return o1[2]-o2[2];
+				}
+			});
+			queue.add(new int[] {0,0,map[0][0]});	// r, c, 가중치
+			while(!queue.isEmpty()) {
+				int[] arr = queue.poll();
+				int r = arr[0];
+				int c = arr[1];
+				int cnt = arr[2];
+				
+				if(minmap[r][c]<cnt) continue;
+				
+				for (int i = 0; i < dirs.length; i++) {
+					int nr = r+dirs[i][0];
+					int nc = c+dirs[i][1];
+					if(nr>=0 && nc >=0 && nr<N && nc <N 
+							&&  cnt+map[nr][nc]<minmap[nr][nc]) {
+						minmap[nr][nc] = cnt+map[nr][nc];
+						queue.add(new int[] {nr,nc,minmap[nr][nc]});
+					}
 				}
 			}
+			System.out.println("Problem "+tc+": "+minmap[N-1][N-1]);
 		}
 	}
-	private static boolean check(int nr, int nc) {
-		// TODO Auto-generated method stub
-		return nr>=0 && nc >=0 && nr < N && nc < N;
-	}
-	
 }
