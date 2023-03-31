3fd722c7d7dc86abb6503e1e8dcbef54fa77da13
CHOEUNBY <72124246+eunbypa@users.noreply.github.com>
2023-03-31 12:27:43 +0900

    [D5] Title: 최장 증가 부분 수열 (Hard), Time: 251 ms, Memory: 92,916 KB -BaekjoonHub

---
+++ SWEA/D5/3308. 최장 증가 부분 수열 （Hard）/최장 증가 부분 수열 （Hard）.java
@@ -0,0 +1,73 @@
+import java.util.*;
+import java.io.*;
+public class Solution {
+	static int[] arr, arr2, record;
+	static int n,max, last;
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			last = 0;
+			max = 0;
+			n = Integer.parseInt(br.readLine());
+			arr = new int[n];
+			arr2 = new int[n];
+			record = new int[n];
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < n; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			//Arrays.sort(arr);
+			findLIS();
+			max = last;
+			sb.append("#"+test_case+" "+max+"\n");
+		}
+		System.out.println(sb.toString());
+	}
+	static void findLIS() {
+		for (int i = 0; i < n; i++) {
+			if(last == 0) {
+				arr2[0] = arr[i];
+				record[i] = 0;
+				last++;
+				continue;
+			}
+			int idx = BS(arr[i]);
+			if(idx < 0) {
+				idx = -(idx);
+			}
+			arr2[idx] = arr[i];
+			record[i] = idx;
+			if(idx == last) {
+				last++;
+			}
+			//System.out.println(Arrays.toString(arr2));
+		}
+	}
+	static int BS(int n) {
+		int idx = -1;
+		int s = 0;
+		int e = last-1; // arr2 배열에서 가장 마지막 원소가 있는 곳 
+		int mid = (s+e)/2;
+		while(s <= e) {
+			mid = (s+e)/2;
+			if(n < arr2[mid]) { // mid 위치 값보다 찾으려는 값이 작으면
+				e = mid-1;
+			}else if(n > arr2[mid]) { // mid 위치 값보다 찾으려는 값이 크면
+				s = mid+1;
+			}else { // 찾으려는 값이 mid 위치 값임
+				idx = mid;
+				break;
+			}
+		}
+		if(idx == -1) {
+			if(e < mid) idx = -mid;
+			else idx = -(mid+1);
+		}
+		return idx;
+	}
+}
