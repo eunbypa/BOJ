0120b560afb1cc1346268e5add6810eeae2f57d6
CHOEUNBY <72124246+eunbypa@users.noreply.github.com>
2023-04-03 13:39:13 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 3,262 ms, Memory: 96,456 KB -BaekjoonHub

---
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+public class Solution {
+	static int N,min;
+	static int[][] edge;
+	static int[][] dp;
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T;
+		T=Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			min = Integer.MAX_VALUE;
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			dp = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				Arrays.fill(dp[i], 10000);
+				for (int j = 0; j < N; j++) {
+					dp[i][j] = Integer.parseInt(st.nextToken());
+					if(dp[i][j] == 0) dp[i][j] = 10000;
+				}
+			}
+			floidWashall();
+			min = findMinCC();
+			sb.append("#"+test_case+" "+min+"\n");
+		}
+		System.out.println(sb.toString());
+	}
+	static void floidWashall() {
+		for (int k = 0; k< N;k++) {
+			for (int i= 0; i < N; i++) {
+				if(k==i) continue;
+				for (int j = 0; j < N; j++) {
+					if(j == k || j == i) continue;
+					dp[i][j] = Math.min(dp[i][j], dp[i][k]+dp[k][j]);
+				}
+			}
+		}
+	}
+	static int findMinCC() {
+		int min = Integer.MAX_VALUE;
+		int sum = 0;
+		for (int i = 0; i < N; i++) {
+			 sum = 0;
+			for (int j = 0; j < N; j++) {
+				if(i==j) continue;
+				sum+=dp[i][j];
+			}
+			min = Math.min(min,sum);
+		}
+		return min;
+	}
+}
