0590c528b99e8630415360fd74874750469c8113
CHOEUNBY <72124246+eunbypa@users.noreply.github.com>
2023-06-15 22:35:04 +0900

    [Silver II] Title: 베르트랑 공준, Time: 128 ms, Memory: 12864 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -4,19 +4,22 @@ import java.io.InputStreamReader;
 
 public class Main {
     static StringBuilder sb = new StringBuilder();
-    static int n, cnt;
+    static int n, cnt, max = 2 * 123456;
+    static int[] dp; // 각 숫자까지의 소수의 개수 저장
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        dp = new int[max+1];
+        for (int i = 2; i <= max; i++) {
+            if(isPrime(i)){
+                dp[i] = dp[i-1]+1;
+            }else dp[i] = dp[i-1];
+        }
         n = Integer.parseInt(br.readLine()); // 테스트 케이스
         int s, e;
         while(n != 0){
-            s = n+1;
+            s = n;
             e = 2*n;
-            for (int i = s; i <= e; i++) {
-                if(isPrime(i)){ // 소수
-                    cnt++;
-                }
-            }
+            cnt = dp[e] - dp[s]; // 2*n 까지의 소수 개수에서 n 까지의 소수 개수를 빼면 범위 내 소수의 개수가 나온다!
             sb.append(cnt+"\n");
             cnt = 0;
             n = Integer.parseInt(br.readLine());
