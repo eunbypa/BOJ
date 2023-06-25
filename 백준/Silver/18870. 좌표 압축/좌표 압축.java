8e31017378f808ee9b6418098c913fec059c5d42
CHOEUNBY <72124246+eunbypa@users.noreply.github.com>
2023-06-25 16:48:02 +0900

    [Silver II] Title: 좌표 압축, Time: 2324 ms, Memory: 366268 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static List<Integer> list = new ArrayList<>();
+    static PriorityQueue<Integer> pq = new PriorityQueue<>();
+    static Map<Integer, Integer> map = new HashMap<>(); // 중복 확인용 & 정렬되었을 때 위치 저장용
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int X;
+        for (int i = 0; i < N; i++) {
+            X = Integer.parseInt(st.nextToken());
+            list.add(X); // 순서대로 Xn 저장
+            if(!map.containsKey(X)) {
+                // 중복된 숫자가 아니면
+                map.put(X, 1);
+                pq.offer(X);
+            }
+        }
+        int cnt = 0;
+        while(!pq.isEmpty()){
+            map.put(pq.poll(), cnt++);
+        }
+        for (int i = 0; i < N; i++) {
+            sb.append(map.get(list.get(i)));
+            if(i == N-1) continue;
+            sb.append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+
+
+}
