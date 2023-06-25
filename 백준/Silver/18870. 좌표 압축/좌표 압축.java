1484663d3415f6b13f830f4bd235e088f1cdb633
CHOEUNBY <72124246+eunbypa@users.noreply.github.com>
2023-06-25 16:59:53 +0900

    [Silver II] Title: 좌표 압축, Time: 1700 ms, Memory: 362892 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -5,29 +5,30 @@ import java.util.*;
 
 public class Main {
     static int N;
-    static List<Integer> list = new ArrayList<>();
-    static List<Integer> sorted = new ArrayList<>();
+    static int[] list, sorted;
     static Map<Integer, Integer> map = new HashMap<>(); // 정렬되었을 때 위치 저장용
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
+        list = new int[N];
+        sorted = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         int X;
         for (int i = 0; i < N; i++) {
             X = Integer.parseInt(st.nextToken());
-            list.add(X); // 순서대로 Xn 저장
-            sorted.add(X);
+            list[i] = X;
+            sorted[i] = X;
         }
         int cnt = 0;
-        Collections.sort(sorted);
+        Arrays.sort(sorted);
         for (int i = 0; i < N; i++) {
-            X = sorted.get(i);
+            X = sorted[i];
             if(!map.containsKey(X)) map.put(X, cnt++);
         }
 
         for (int i = 0; i < N; i++) {
-            sb.append(map.get(list.get(i)));
+            sb.append(map.get(list[i]));
             if(i == N-1) continue;
             sb.append(" ");
         }
