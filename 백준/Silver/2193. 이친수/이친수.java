799d6c0df7a6f7c6465b42b67645a56c822fef74
CHOEUNBY <72124246+eunbypa@users.noreply.github.com>
2023-08-07 22:20:31 +0900

    [Silver III] Title: 이친수, Time: 76 ms, Memory: 11492 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static long[][] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        dp = new long[N+1][2];
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+        for (int i = 2; i <= N; i++) {
+            dp[i][0] = dp[i-1][0] + dp[i-1][1];
+            dp[i][1] = dp[i-1][0];
+        }
+        long result = dp[N][0] + dp[N][1];
+        System.out.println(result);
+    }
+
+
+}
