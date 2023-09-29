ebcf25814b724d3f09a82204b8d18edf85e1da4e
CHOEUNBY <72124246+eunbypa@users.noreply.github.com>
2023-09-29 22:19:44 +0900

    [Silver IV] Title: 보물, Time: 76 ms, Memory: 11616 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int[] A;
+    static int[] B;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        A = new int[N];
+        B = new int[N];
+        int min = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+        // 오름차순 정렬
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for (int i = 0; i < N; i++) {
+            min += A[i] * B[N - 1 - i];
+        }
+        System.out.println(min);
+    }
+}
