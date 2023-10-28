546b30b35b8610d88e5806db021da5a29c5fc403
CHOEUNBY <72124246+eunbypa@users.noreply.github.com>
2023-10-28 22:17:56 +0900

    [Silver III] Title: 모든 순열, Time: 168 ms, Memory: 20800 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.java
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -1,42 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-
     static int N;
-    static int[] choices;
-    static boolean[] visited;
-    static StringBuilder result = new StringBuilder();
-
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        choices = new int[N];
-        visited = new boolean[N + 1];
-
-        dfs(0);
-
-        System.out.println(result);
-    }
-
-    static void dfs(int cnt) {
-        if (cnt == N){
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = i+1;
+        }
+        do {
             for (int i = 0; i < N; i++) {
-                result.append(choices[i]).append(" ");
+                sb.append(arr[i]);
+                if(i == N-1) break;
+                sb.append(" ");
             }
-            result.append("\n");
-            return;
+            sb.append("\n");
+        } while (nextPerm());
+        System.out.println(sb.toString());
+    }
+
+    public static boolean nextPerm() {
+        int i = N-1;
+        int j = N-1;
+        while(i > 0 && arr[i-1] > arr[i]) i--;
+        if(i == 0) return false;
+        while(arr[i-1] > arr[j]) j--;
+        swap(i-1, j);
+        j = N-1;
+        while(i < j) {
+            swap(i, j);
+            i++;
+            j--;
         }
 
-        for (int i = 1; i <= N; i++) {
-            if (visited[i]) {
-                continue;
-            }
-            visited[i] = true;
-            choices[cnt] = i;
-            dfs(cnt + 1);
-            visited[i] = false;
-        }
+        return true;
     }
+
+    public static void swap(int i, int j){
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+
 }
