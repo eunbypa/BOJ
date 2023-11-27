4aefa9be242ae06717c0815579bf68cd11fa6227
CHOEUNBY <72124246+eunbypa@users.noreply.github.com>
2023-11-27 22:09:59 +0900

    [Silver V] Title: K번째 수, Time: 3720 ms, Memory: 664712 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N,K;
+    static List<Integer> list;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        list = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        Collections.sort(list);
+        System.out.println(list.get(K-1));
+    }
+
+
+
+}
