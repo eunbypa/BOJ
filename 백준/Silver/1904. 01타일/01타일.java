ef265585ae96d49971a16c8ffa3735abcc73c863
CHOEUNBY <72124246+eunbypa@users.noreply.github.com>
2024-06-24 22:08:26 +0900

    [Silver III] Title: 01타일, Time: 100 ms, Memory: 15444 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static final int MOD = 15746;
+    static int n;
+    static int[] dp;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 2] % MOD + dp[i - 1] % MOD) % MOD;
+        }
+        System.out.println(dp[n]%MOD);
+    }
+
+}
